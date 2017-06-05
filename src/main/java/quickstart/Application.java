3cf4f1fe8fbfdb93dfb8ade0afe9c0f7fd22536b
Maxim Tolstykh <maxim.tolstyh@ge.com>
2017-06-05 13:25:25 +0300

    add runable jar/war build task

--- src/main/java/quickstart/Application.java
+++ src/main/java/quickstart/Application.java
@@ -2,11 +2,18 @@ package quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
